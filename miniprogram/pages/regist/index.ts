119c80eab7ad89a28dd757dc6c247ca0fbb10d25
cashon1120 <49798613@qq.com>
2022-09-27 17:53:17 +0800

    平台注册:添加二维码图片

--- miniprogram/pages/regist/index.ts
+++ miniprogram/pages/regist/index.ts
@@ -4,7 +4,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-    images: [{ name: '环旅出行', src: '/images/code_1.jpg' }, { name: '365约车', src: '/images/code_2.jpg' }]
+    images: [{ name: '首汽', src: '/images/code_shouqi.png' },{ name: '环旅出行', src: '/images/code_hlcx.jpg' }, { name: '365约车', src: '/images/code_365.jpg' }, { name: '阳光出行', src: '/images/code_yangguang.jpg' }]
   },
   handleShowImg(e: any){
     const url = e.currentTarget.dataset.url
