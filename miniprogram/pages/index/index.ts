348c9b941b4849be03754d603b5ec62990f5e6db
cashon1120 <49798613@qq.com>
2022-09-27 18:00:17 +0800

    会议会展界面

--- miniprogram/pages/index/index.ts
+++ miniprogram/pages/index/index.ts
@@ -18,7 +18,7 @@ Page({
       { url: '', src: '../../images/icon_4.png', label: '租房' },
       { url: 'lawService', src: '../../images/icon_6.png', label: '法律服务' },
       // { url: 'driverAccreditation/default', src: '../../images/icon_3.png', label: '司机办证' },
-      { url: '', src: '../../images/icon_8.png', label: '会议会展' },
+      { url: 'meeting', src: '../../images/icon_8.png', label: '会议会展' },
       { url: '', src: '../../images/icon_9.png', label: '生活服务' },
       // { url: 'education', src: '../../images/icon_1.png', label: '安全教育' },
       // { url: 'taxi/default', type: 1, src: '../../images/icon_7.png', label: '打车(直接跳)' },
---
+++ miniprogram/pages/meeting/index.ts
@@ -0,0 +1,64 @@
+import { DriverAccreditation } from '../../api/index'
+const app = getApp()
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    formItem: [
+      { label: '您的姓名', name: 'driverName', value: '', placeholder: '请输入您的姓名', type: 'input', required: { message: '请输入您的姓名' } },
+      { label: '您的手机号', maxLength: 11, name: 'phone', value: '', placeholder: '请输入您的手机号', type: 'input', inputType: 'number', required: { message: '请输入您的手机号' }, validate: { message: '请输入正确的11位手机号', exec: (phone: string) => /^1\d{10}$/.test(phone) } },
+      { label: '白色背景半身照', name: 'photo', value: '', type: 'upload', required: { message: '请上传白色背景半身照' } },
+      { label: '身份证', inline: true, name: 'identityCardFont', value: '', type: 'upload', uploadText: '上传正页', required: { message: '请上传身份证正页' }, template: 1, des: '请上传身份证正页' },
+      { label: '', inline: true, name: 'identityCardBack', value: '', type: 'upload', uploadText: '上传副页', required: { message: '请上身份证副页' }, template: 2, des: '请上身份证副页' },
+      { label: '驾驶证', inline: true, name: 'driverLicenceFont', value: '', type: 'upload', uploadText: '上传正页', required: { message: '请上传驾驶证正页' }, template: 3, des: '请上传驾驶证正页' },
+      { label: '', inline: true, name: 'driverLicenceBack', value: '', type: 'upload', uploadText: '上传副页', required: { message: '请上传驾驶证副页' }, template: 4, des: '请上传驾驶证副页' },
+      { label: '居住证(成都市外)', name: 'residencePermit', value: '', type: 'upload' },
+      { label: '无犯罪记录证明', name: 'otherCertificateList', value: '', type: 'upload', count: 6 },
+    ],
+    loading: false
+  },
+  onLoad() {
+    app.globalData.uploadUrl = 'rentOnlineDriverLicenceApplication/upload'
+  },
+  handleFormChange(e: any) {
+    this.setData({
+      formItem: e.detail
+    })
+  },
+  handleSubmit(e: any) {
+    this.setData({
+      loading: true
+    })
+    DriverAccreditation({ ...e.detail, rentUserId: wx.getStorageSync('userID') }).then((res: any) => {
+      if (res.code === 0) {
+        switch (res.data.code) {
+          case 0:
+            wx.navigateBack()
+            wx.showToast({
+              title: '申请成功',
+              icon: 'none'
+            })
+            break;
+          case 1:
+            wx.showToast({
+              title: '申请失败',
+              icon: 'none'
+            })
+            break;
+          case 2:
+            wx.showToast({
+              title: '您已申请',
+              icon: 'none'
+            })
+            break;
+        }
+      }
+    }).finally(() => {
+      this.setData({
+        loading: false
+      })
+    })
+  },
+})
