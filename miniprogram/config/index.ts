871fcffddd6ab75f02521df3c21c5bcad5580a98
cashon1120 <49798613@qq.com>
2022-10-08 17:23:17 +0800

    细节调整

--- miniprogram/config/index.ts
+++ miniprogram/config/index.ts
@@ -1,21 +1,17 @@
 /**
  * ----------上传前注意事项 ----------
  * 1: 修改接口地址为正式环境: API_URL;
- * 4: 看有没有多余的console.log;
- * 5: 搜索"调试"或"测试"字样, 看有没有专门调试用的代码;
+ * 2: 看有没有多余的console.log;
+ * 3: 搜索"调试"或"测试"字样, 看有没有专门调试用的代码;
 */
 
 // 正式接口地址
-// export const API_URL = 'https://cacheapi.emhes.cn/'
+export const API_URL = 'https://wyc-api.chengduwangyueche.com/'
 
 // 测试接口地址
-// export const API_URL = 'https://wycapi.hi515.cn/'
-export const API_URL = 'http://192.168.10.99:9002/'
+// export const API_URL = 'https://wyc-api.chengduwangyueche.com/'
+// export const API_URL = 'http://192.168.10.99:9002/'
 // export const API_URL = 'http://182.150.24.182:13415/'
-
-
-// 腾讯地图key
-export const TMAP_KEY = 'DTRBZ-AJOWX-XBJ4U-TNQ6U-BUYH6-37F3J'
 
 // 快电渠道号
 export const KD_PT = '922090901'
