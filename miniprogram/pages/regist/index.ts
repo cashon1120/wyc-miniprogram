e3b7f812ac73d1eb71e32de1fd5074f3abea8196
cashon1120 <49798613@qq.com>
2022-10-12 11:40:56 +0800

    添加注册平台

--- miniprogram/pages/regist/index.ts
+++ miniprogram/pages/regist/index.ts
@@ -4,7 +4,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-    images: [{ name: '首汽约车', src: '/images/code_shouqi.png' },{ name: '环旅出行', src: '/images/code_hlcx.jpg' }, { name: '365约车', src: '/images/code_365.jpg' }, { name: '阳光出行', src: '/images/code_yangguang.jpg' }, { name: 'T3出行', src: '/images/code_t3.png' }]
+    images: [{ name: '首汽约车', src: '/images/code_shouqi.png' },{ name: '环旅出行', src: '/images/code_hlcx.jpg' }, { name: '365约车', src: '/images/code_365.jpg' }, { name: '阳光出行', src: '/images/code_yangguang.jpg' }, { name: 'T3出行', src: '/images/code_t3.png' }, { name: '美团打车', src: '/images/code_meituan.png' },{ name: '曹操出行', src: '/images/code_caocao.png' }]
   },
 
   handleShowImg(e: any){
